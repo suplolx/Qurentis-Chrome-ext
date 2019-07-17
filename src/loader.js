eb5564d84f2356d4584a5a2b623a0d1453a1238a
Hans Beijk <Hans@Kr8tig.ict>
2019-07-17 15:39:34 +0200

    Implemented save functionality

--- src/loader.js
+++ src/loader.js
@@ -1,8 +1,7 @@
 if (window.location.href.split("/")[3] === "ReportSupportingReport") {
-    // Elementen selecteren om in te injecteren
+  // Elementen selecteren om in te injecteren
   let formGroup = document.querySelectorAll(".form-group")[1].firstElementChild;
   let body = document.querySelector("body");
-
 
   // Elementen maken van de modal en de buttons
   let button = document.createElement("button");
@@ -15,6 +14,7 @@ if (window.location.href.split("/")[3] === "ReportSupportingReport") {
   let modalFooter = document.createElement("div");
   let textArea = document.createElement("textarea");
   let copyBtn = document.createElement("button");
+  let savedBtn = document.createElement("button");
 
   // Injecteren van aangemaakte elementen
   body.appendChild(modal);
@@ -28,6 +28,17 @@ if (window.location.href.split("/")[3] === "ReportSupportingReport") {
   modalFooter.appendChild(copyBtn);
   modalFooter.appendChild(closeBtn);
   formGroup.appendChild(button);
+
+  if (localStorage.getItem("saved")) {
+    modalFooter.appendChild(savedBtn);
+
+    savedBtn.className = "btn btn-warning";
+    savedBtn.innerHTML = "Laatst opgeslagen tekst";
+
+    savedBtn.addEventListener("click", function() {
+      textArea.value = localStorage.getItem("saved");
+    })
+  }
 
   // Classes en id's geven aan elementen voor css
   modal.className = "modal";
@@ -66,11 +77,16 @@ if (window.location.href.split("/")[3] === "ReportSupportingReport") {
     modal.style.display = "none";
   })
 
+  textArea.addEventListener("keyup", function() {
+    localStorage.setItem("saved", textArea.value)
+  })
+
 } else {
   
-  var fullName = document.querySelector(".col-md-9").firstElementChild.innerHTML.split(" ");
+  var nameElement = document.querySelector(".col-md-9");
+  var fullName = nameElement.firstElementChild.innerHTML.split(" ");
   var firstName = fullName.slice(0, fullName.length - 1).join(" ");
-  var lastName = fullName[fullName.length - 1]
+  var lastName = fullName[fullName.length - 1];
 
   console.log(firstName, lastName);
   
@@ -81,8 +97,8 @@ if (window.location.href.split("/")[3] === "ReportSupportingReport") {
   
     .then(response => {
      var newArray = response.Data.filter(function(el) {
-       return el.Firstname == "Menno" && el.Lastname == "Hendrix"
+       return el.Firstname == firstName && el.Lastname == lastName
      })
-     console.log(newArray);
+     nameElement.firstElementChild.innerHTML = `<a style="color:inherit;" href="/Clients/Details/${newArray[0].ID}">${firstName} ${lastName}</a>`;
     });
 }
